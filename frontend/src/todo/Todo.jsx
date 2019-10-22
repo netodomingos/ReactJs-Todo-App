f4a5a9c6dc8cbc2216e894cc1753f329d4926ff0
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2019-10-22 13:05:45 -0300

    Adicionamento e Exclusão dos dados

--- frontend/src/todo/Todo.jsx
+++ frontend/src/todo/Todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
+
 import PageHeader from '../template/PageHeader'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList';
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class todo extends Component {
     constructor(props) {
@@ -12,6 +16,16 @@ export default class todo extends Component {
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
+    }
+    refresh() {
+        Axios.get(`${URL}?sort=-createdAt`)
+            .then(res => this.setState({
+                ...this.state,
+                description: '',
+                list: res.data
+            }))
     }
     handleChange(e) {
         this.setState({
@@ -20,7 +34,14 @@ export default class todo extends Component {
         })
     }
     handleAdd() {
+        const description = this.state.description
+        Axios.post(URL, { description })
+            .then(res => this.refresh())
 
+    }
+    handleRemove(todo) {
+        Axios.delete(`${URL}/${todo._id}`)
+            .then(res => this.refresh())
     }
     render() {
         return (
@@ -31,7 +52,7 @@ export default class todo extends Component {
                     description={this.state.description}
                     handleChange={this.handleChange}
                 />
-                <TodoList />
+                <TodoList list={this.state.list} handleRemove={this.handleRemove} />
             </div>
         )
     }
--- frontend/src/todo/TodoList.jsx
+++ frontend/src/todo/TodoList.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import Button from '../template/Button'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <Button
+                        style='danger'
+                        icon='trash-o'
+                        onClick={() => props.handleRemove(todo)}
+                    />
+                </td>
+            </tr>
+        ))
+
+    }
+
+    return (
+        <div className='mt-4'>
+            <table className='table'>
+                <thead>
+                    <td>
+                        <th>Descrição</th>
+                    </td>
+                    <td>
+                        <th>Ações</th>
+                    </td>
+                </thead>
+                <tbody>
+                    {renderRows()}
+                </tbody>
+            </table>
+        </div>
+    )
+
+}
